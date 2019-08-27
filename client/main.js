052bcaedae92724e71c14e7b1c185236db9d4679
Darren Xu <darrenxu1994@yahoo.com.au>
2019-08-27 18:34:30 +1000

    space to hold

--- client/main.js
+++ client/main.js
@@ -37,6 +37,8 @@ function draw() {
         player.dropInterval = player.DROP_FAST;
     } else if (keyIsDown(UP_ARROW)) {
         player.dropInterval = 1;
+    } else if (keyIsDown(32)) {
+        player.dropInterval = 6000;
     } else {
         player.dropInterval = player.DROP_SLOW;
     }
